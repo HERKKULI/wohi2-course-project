e08bd696bc5260e39ff9bf0abbdb6c79ea48ab64
HERKKULI <hjerkku@gmail.com>
2026-04-18 21:35:30 +0300

    Adding PRISMA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
 const questionRouter = require("./routes/questions"); 
+const prisma = require("./lib/prisma");
+
 
 app.use(express.json());
 
@@ -14,4 +15,15 @@ app.use((req, res) => {
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
+});
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
